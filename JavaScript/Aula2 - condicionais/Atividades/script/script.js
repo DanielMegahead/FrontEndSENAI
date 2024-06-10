203300719b8749c9a0e0e5e7b67da91246dd96cf
dannicod <eidaniel@outlook.com>
2024-06-10 15:11:46 -0300

    JavaScript Finalizado

--- JavaScript/Aula2 - condicionais/Atividades/script/script.js
+++ JavaScript/Aula2 - condicionais/Atividades/script/script.js
@@ -1,24 +1,26 @@
-function ex1(){
+function ex1() {
     // receber idade
     // if
     // verificando faixa etaria
     // informar ao usuario
-    let idade = Number(prompt("Digite a sua idade"));
+    let idade = Number(prompt("Digite a sua idade:"));
 
-    if(idade >= 0 & idade < 15){
+    if (idade >= 0 & idade < 15) {
         alert("Criança");
-    }else idade(idade >= 15 & idade < 30){
+    } else if (idade >= 15 & idade < 30) {
         alert("Jovem")
-    }else if(idade >= 30 & idade <60){
+    } else if (idade >= 30 & idade < 60) {
+        alert("Adulto")
+    } else if (idade >= 60) {
         alert("Idoso")
-    }else{
+    } else {
         alert("Idade Inválida")
     }
 }
 
-function ex2(){
+function ex2() {
     let dolares = Number(prompt("Insira a quantidade em dolares"))
-    cost cota = 5.10
+    const cota = 5.10
 
     let reais = dolares * cota
     alert(`O valor em reais é de : ${reais}`)
